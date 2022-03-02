66df912b03bf2fae3d2ae19f7e706ce3b63288de
naofumi.higashikawauchi <naofumi.higashikawauchi@interman.co.jp>
2022-03-02 15:27:28 +0900

    dev

--- askul_item/__init__.py
+++ askul_item/__init__.py
@@ -24,49 +24,48 @@ def get_item_info(product_code:str)->Dict[str, str]:
     item_url = "https://www.askul.co.jp/p/" + product_code + "/"
     for i in range(3):
         try:
-            res = __session__.get(item_url, timeout=3)
-            break
+            res = __session__.get(item_url, timeout=5)
+
+            html = lxml.html.fromstring(res.text)
+            item_info = {"product_code": product_code}
+
+            # 取扱中止等のエラーがある場合は商品名にエラーメッセージを入れて終了
+            if len(html.xpath('//div[@class="errorMessageInner"]'))>0:
+                item_info["item_name"] = html.xpath('//div[@class="errorMessageInner"]')[0].text_content().strip()
+                item_info["success"] = False
+                return item_info
+            
+            head_breadcrumbs = html.xpath('//div[@class="breadcrumbs"]')[0]
+            elms = head_breadcrumbs.xpath('.//ol/li')[1:]
+
+            for i in range(len(elms)):
+                item_info["cat_path_" + str(i)] = elms[i].xpath("./a")[0].attrib["href"]
+                item_info["cat_name_" + str(i)] = elms[i].xpath("./a/span")[0].text
+
+            img_elms = html.xpath('//div[@class="productThumb"]/ul/li/a/img')
+            for i in range(len(img_elms)):
+                item_info["img_url_" + str(i)] ="https:" + img_elms[i].attrib["src"]
+            
+            if len(html.xpath('//span[@class="price"]/span[@class="num"]')) > 0:
+                item_info["price_tax"] = html.xpath('//span[@class="price"]/span[@class="num"]')[0].text.replace("￥","").replace(",","")
+            else:
+                item_info["price_num"] = html.xpath('//p[@class="priceNum"]/span[@class="num"]')[0].text.replace("￥","").replace(",","") if len( html.xpath('//p[@class="priceNum"]/span[@class="num"]')) > 0 else ""
+                item_info["price_tax"] = html.xpath('//p[@class="priceNum"]/span[@class="tax"]')[0].text.replace("￥","").replace(",","") if len( html.xpath('//p[@class="priceNum"]/span[@class="tax"]')) > 0 else ""
+
+            item_info["item_name"] = html.xpath('//h1[@class="productTitle wrongInformationModalTarget-name"]')[0].text.strip().replace("\u3000"," ")
+            item_info["item_format"] =  html.xpath('//span[@class="format"]/span')[0].attrib["content"] if len(html.xpath('//span[@class="format"]/span')) > 0 else ""
+            item_info["item_jancode"] =  html.xpath('//span[@class="janCode"]')[0].text.split("：")[1] if len(html.xpath('//span[@class="janCode"]')) > 0 else ""
+
+            item_info["success"] = True
+            return item_info
+
         except Exception as e:
             print(e)
             pass
     else:
         raise TimeoutError()
 
-    html = lxml.html.fromstring(res.text)
-    item_info = {"product_code": product_code}
-
-    # 取扱中止等のエラーがある場合は商品名にエラーメッセージを入れて終了
-    if len(html.xpath('//div[@class="errorMessageInner"]'))>0:
-        item_info["item_name"] = html.xpath('//div[@class="errorMessageInner"]')[0].text_content().strip()
-        item_info["success"] = False
-        return item_info
-    
-    head_breadcrumbs = html.xpath('//div[@class="breadcrumbs"]')[0]
-    elms = head_breadcrumbs.xpath('.//ol/li')[1:]
-
-    for i in range(len(elms)):
-        item_info["cat_path_" + str(i)] = elms[i].xpath("./a")[0].attrib["href"]
-        item_info["cat_name_" + str(i)] = elms[i].xpath("./a/span")[0].text
-
-    img_elms = html.xpath('//div[@class="productThumb"]/ul/li/a/img')
-    for i in range(len(img_elms)):
-        item_info["img_url_" + str(i)] ="https:" + img_elms[i].attrib["src"]
-    
-    if len(html.xpath('//span[@class="price"]/span[@class="num"]')) > 0:
-        item_info["price_tax"] = html.xpath('//span[@class="price"]/span[@class="num"]')[0].text.replace("￥","").replace(",","")
-    else:
-        item_info["price_num"] = html.xpath('//p[@class="priceNum"]/span[@class="num"]')[0].text.replace("￥","").replace(",","") if len( html.xpath('//p[@class="priceNum"]/span[@class="num"]')) > 0 else ""
-        item_info["price_tax"] = html.xpath('//p[@class="priceNum"]/span[@class="tax"]')[0].text.replace("￥","").replace(",","") if len( html.xpath('//p[@class="priceNum"]/span[@class="tax"]')) > 0 else ""
-
-    item_info["item_name"] = html.xpath('//h1[@class="productTitle wrongInformationModalTarget-name"]')[0].text.strip().replace("\u3000"," ")
-    item_info["item_format"] =  html.xpath('//span[@class="format"]/span')[0].attrib["content"] if len(html.xpath('//span[@class="format"]/span')) > 0 else ""
-    item_info["item_jancode"] =  html.xpath('//span[@class="janCode"]')[0].text.split("：")[1] if len(html.xpath('//span[@class="janCode"]')) > 0 else ""
-
-    item_info["success"] = True
-
-    return item_info
-
 
 if __name__ == "__main__":
-    item = get_item_info("2020174")
+    item = get_item_info("8210846")
     print(item)
