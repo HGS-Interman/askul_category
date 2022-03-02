ee45822aa6f207623b292425144a726ab297d226
naofumi.higashikawauchi <naofumi.higashikawauchi@interman.co.jp>
2022-03-02 12:51:38 +0900

    dev

--- askul_item/__init__.py
+++ askul_item/__init__.py
@@ -7,6 +7,9 @@ from single_source import get_version
 
 __version__ = get_version(__name__, Path(__file__).parent.parent)
 
+# 医療機関であるかの確認画面に遷移する対策
+__session__ = requests.session() 
+__session__.post("https://www.askul.co.jp/ctg/medical/medicalConfirm/clickYes", data = {'backUrl': '/p/1596710/'})
 
 def get_item_info(product_code:str)->Dict[str, str]:
     """
@@ -21,15 +24,23 @@ def get_item_info(product_code:str)->Dict[str, str]:
     item_url = "https://www.askul.co.jp/p/" + product_code + "/"
     for i in range(3):
         try:
-            res = requests.get(item_url, timeout=3)
+            res = __session__.get(item_url, timeout=3)
             break
-        except:
+        except Exception as e:
+            print(e)
             pass
     else:
         raise TimeoutError()
 
     html = lxml.html.fromstring(res.text)
     item_info = {"product_code": product_code}
+
+    # 取扱中止等のエラーがある場合は商品名にエラーメッセージを入れて終了
+    if len(html.xpath('//div[@class="errorMessageInner"]'))>0:
+        item_info["item_name"] = html.xpath('//div[@class="errorMessageInner"]')[0].text_content().strip()
+        item_info["success"] = False
+        return item_info
+    
     head_breadcrumbs = html.xpath('//div[@class="breadcrumbs"]')[0]
     elms = head_breadcrumbs.xpath('.//ol/li')[1:]
 
@@ -38,20 +49,21 @@ def get_item_info(product_code:str)->Dict[str, str]:
         item_info["cat_name_" + str(i)] = elms[i].xpath("./a/span")[0].text
 
     img_elms = html.xpath('//div[@class="productThumb"]/ul/li/a/img')
-
     for i in range(len(img_elms)):
         item_info["img_url_" + str(i)] ="https:" + img_elms[i].attrib["src"]
 
-    item_info["price_num"] = html.xpath('//p[@class="priceNum"]/span[@class="num"]')[0].text.replace("￥","").replace(",","")
-    item_info["price_tax"] = html.xpath('//p[@class="priceNum"]/span[@class="tax"]')[0].text.replace("￥","").replace(",","")
+    item_info["price_num"] = html.xpath('//p[@class="priceNum"]/span[@class="num"]')[0].text.replace("￥","").replace(",","") if len( html.xpath('//p[@class="priceNum"]/span[@class="num"]')) > 0 else ""
+    item_info["price_tax"] = html.xpath('//p[@class="priceNum"]/span[@class="tax"]')[0].text.replace("￥","").replace(",","") if len( html.xpath('//p[@class="priceNum"]/span[@class="tax"]')) > 0 else ""
     item_info["item_name"] = html.xpath('//h1[@class="productTitle wrongInformationModalTarget-name"]')[0].text.strip().replace("\u3000"," ")
     item_info["item_format"] =  html.xpath('//span[@class="format"]/span')[0].attrib["content"] if len(html.xpath('//span[@class="format"]/span')) > 0 else ""
-    item_info["item_jancode"] =  html.xpath('//span[@class="janCode"]')[0].text.split("：")[1]
+    item_info["item_jancode"] =  html.xpath('//span[@class="janCode"]')[0].text.split("：")[1] if len(html.xpath('//span[@class="janCode"]')) > 0 else ""
+
+    item_info["success"] = True
 
     return item_info
 
 
 if __name__ == "__main__":
     import pdb; pdb.set_trace()
-    item = get_item_info("688881")
+    item = get_item_info("1596710")
     print(item)
